b1446fee43585a2e5ddde62b8ad522bd33fc64a6
madhuachuth <madhuachuth7@gmail.com>
2023-06-12 09:49:30 +0000

    github

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,20 @@
+// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {languageDetails, updateQueryParams, isActive} = props
+  const {language, id} = languageDetails
+  const onUpdateParams = () => {
+    updateQueryParams(id)
+  }
+  const btnStyle = isActive ? 'normal selected' : 'normal'
+  return (
+    <li>
+      <button type="button" className={btnStyle} onClick={onUpdateParams}>
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
